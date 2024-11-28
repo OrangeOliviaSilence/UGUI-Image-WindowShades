27960e76e248a39e513c28489e4575914a66d198
OrangeJoseSlience <137153189@qq.com>
2024-11-28 15:12:18 +0800

    添加代码：打印出UniTask的所有插入到Unity默认PlayerLoop的SubSystem

--- Test Project 1/Assets/Scripts/UniTask学习/UniTaskTest3.cs
+++ Test Project 1/Assets/Scripts/UniTask学习/UniTaskTest3.cs
@@ -20,7 +20,20 @@ public class UniTaskTest3 : MonoBehaviour, IProgress<float>
         AsyncGetWebInfo(destroyCancellationToken).Forget();
 		PlayerLoopSystem currentLoop = PlayerLoop.GetCurrentPlayerLoop();
         for (int i = 0; i < currentLoop.subSystemList.Length; i++)
-            Debug.Log($"PlayerLoop Phase[{i}]: {currentLoop.subSystemList[i]}");
+        {
+            var subsystem = currentLoop.subSystemList[i];
+			Debug.Log($"PlayerLoop Phase[{i}]: {subsystem}");
+
+            // ´òÓ¡³öSubSubSystem
+            if (subsystem.subSystemList != null)
+            {
+				for (int j = 0; j < subsystem.subSystemList.Length; j++)
+				{
+                    var subSubSystem = subsystem.subSystemList[j];
+					Debug.Log($"PlayerLoop Phase[{i}]: {subsystem}: [{j}]: {subSubSystem}");
+				}
+			}
+		}
 	}
 
     // Update is called once per frame
